11386f430543c397a63a5a381301067d2a008847
valiantlynx <86688436+valiantlynx@users.noreply.github.com>
2022-06-08 20:08:04 +0200

    map, filter, reduce, find and findIndex functions

--- src/index.js
+++ src/index.js
@@ -1,11 +1,42 @@
 var numbers = [3, 56, 2, 48, 5];
 
-//Map -Create a new array by doing something with each item in an array.
+//****** */Map -Create a new array by doing something with each item in an array.
+//****** */does basicly the same as a foreach loop without the hustle of creating an empy array and pushis stuff into it for each itteration
 
-//Filter - Create a new array by keeping the items that return true.
+// function double(x) {
+//   return x * 2;
+// }
+// const newNumber = numbers.map(double);
+// console.log(newNumber);
 
-//Reduce - Accumulate a value by doing something to each item in an array.
+//****** */Filter - Create a new array by keeping the items that return true.
 
-//Find - find the first item that matches from an array.
+// const newNumber = numbers.filter(function (x) {
+//   return x > 10;
+// });
 
-//FindIndex - find the index of the first item that matches.
+// console.log(newNumber);
+
+//***** */Reduce - Accumulate a value by doing something to each item in an array.
+
+// const newNumber = numbers.reduce(function (accumulator, currentNumber) {
+//   console.log("accumulator = " + accumulator);
+//   console.log("currentNumber = " + currentNumber);
+//   return accumulator + currentNumber;
+// });
+
+// console.log(newNumber);
+
+//****** */Find - find the first item that matches from an array.
+
+// const newNumber = numbers.find(function (num) {
+//   return num > 10;
+// });
+// console.log(newNumber);
+
+//****** */FindIndex - find the index of the first item that matches.
+
+const newNumber = numbers.findIndex(function (num) {
+  return num > 10;
+});
+console.log(newNumber);
